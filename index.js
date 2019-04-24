6599d0b5d4b73cce36d90952cb7bd2b3d2887b46
BlancaCarrasco <blankcarr@gmail.com>
2019-04-24 16:07:54 -0400

    lee readme saca los links y da el estatus del link

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 #!/usr/bin/env node
-// module.exports = () => {
-//   // ...
-// };
-// const math = require('./src/math');
-// console.log(math.add(2,2))
-// console.log(math.multiply(3,5))
-const fs= require ('fs');
+
+const fs= require ('fs');// Permite manejar archivos
 const markdownLinkExtractor = require('markdown-link-extractor');
+const path = require('path');
+const mdlinks = require('./mdlinks');
+const chalk = require('chalk'); // Le da color al texto en la consola
+const figlet = require('figlet'); // Texto bonito para titulo
 
+const fetch = require('fetch');
+const fetchUrl= fetch.fetchUrl;
 
-// fs.readFile ('README.md','utf-8',(error, data)=>{
-//   if (error){
-//     console.log(`Error ${error}`);
-//   }else{
-//     console.log(data);
-//   }
-// })
+const validate = process.argv.indexOf("--validate");
 
+const init = () => {
+    console.log(
+        chalk.yellow(
+            figlet.textSync("MD-LINKS", {
+                font: "Contrast",
+                horizontalLayout: "default",
+                verticalLayout: "default"
+            })
+        )
+    );
+  }; 
+  init();    // Imprimimos pantalla de inicio
 
+const mdLinks = (path) => {
+    // Validar la ruta
+    if (fs.exists(path)) {
+        console.log('existe la ruta');
+        // Verificamos que sea un archivo, por que una ruta puede ser valida, pero ser una carpeta
+        if (fs.stat(path).isFile()) {
+            // Verificamos que sea un archivo .md
+            const arrCadenas = path.split('.');
+            if (arrCadenas[1] === 'md') {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+fs.readFile ('README.md','utf-8',(error, data)=>{
+  if (error){
+    console.log(error);
+  }else{
+    console.log(data);
+  }
+})
 
 let markdown = fs.readFileSync('README.md').toString();
 
@@ -25,5 +55,27 @@ let links = markdownLinkExtractor(markdown);
 
 links.forEach(function (link) {
     console.log(link);
-});
 
+const getData = (link)=>{
+    return new Promise((resolve,reject)=>{
+      fetchUrl(link,(error,meta,body)=>{
+        if (meta){
+          if (meta){
+            resolve(meta.status.toString());
+          }
+        }else{
+          reject(error);
+        }
+      })
+    })
+  }
+  let links="link"
+getData(link)
+    .then(res=>{
+      if(res==200)
+      console.log("el estado de",link,"es:",res);
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+})
--- mdlinks.js
+++ mdlinks.js
@@ -1,7 +1,64 @@
-// function mdlinks (){
+#!/usr/bin/env node
 
-// }
+const chalk = require('chalk'); // Le da color al texto en la consola
+const figlet = require('figlet'); // Texto bonito para titulo
+const minimist = require('minimist'); // Permite leer parametros al ejecutar
+const fs = require('fs'); // Permite manejar archivos
+// const fetch = require('node-fetch');
+var marked = require('marked');
 
-// module.export={
+
+// const errorYFinalizar = (texto) => {
+//   console.log(chalk.red('Error: ', texto, '\existe un error'));
+//   process.exit(0);
+// };
+
+// const mdlinks = (path) => {
+//   // Validar la ruta
+//   if (fs.exists(path)) {
+//       console.log('existe la ruta');
+//       // Verificamos que sea un archivo, por que una ruta puede ser valida, pero ser una carpeta
+//       if (fs.stat(path).isFile()) {
+//           // Verificamos que sea un archivo .md
+//           const arrCadenas = path.split('.');
+//           if (arrCadenas[1] === 'md') {
+//               return true;
+//           }
+//       }
+//       return false;
+//   }
+// };
+// const getParams = () => {
+//   const args = minimist(process.argv.slice(2));
+//   let parametros = {
+//       validate: [ ],
+//       stats: [ ],
+//       file: [ ]
+//   };
+//   if (args._.length) {
+//       args._.forEach((url) => {
+//           const existe = mdLinks(url);
+//           if (!existe) {
+//               errorYFinalizar(`Archivo invalido: ${url}`);
+//           }
+//           parametros.file.push(url);
+//       })
+//   } else {
+//       errorYFinalizar('Debe haber al menos un archivo');
+//   }
+//   if (args.validate) {
+//       parametros.validate = validate;
+//   }
+//   if (args.stats) {
+//       parametros.stats = stats
+//   }
+//   return parametros;
+// };
+
+
+// module.exports = {
+ 
+//   getParams,
 //   mdlinks
-// }
+  
+// };
